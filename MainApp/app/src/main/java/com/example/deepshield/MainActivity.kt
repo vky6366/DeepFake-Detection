75f52397a411972b73c94dd95a6d6499d8b04739
Akshay Sarapure <akshaysarapure@gmail.com>
2025-02-17 12:31:56 +0530

    Constants Updated

--- MainApp/app/src/main/java/com/example/deepshield/MainActivity.kt
+++ MainApp/app/src/main/java/com/example/deepshield/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.deepshield.Constants.Constants
 import com.example.deepshield.ui.theme.DeepShieldTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,7 +34,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello ${Constants.API_KEY}!",
         modifier = modifier
     )
 }
