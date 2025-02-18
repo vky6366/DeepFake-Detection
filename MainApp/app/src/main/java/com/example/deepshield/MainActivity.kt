b92ca1135d6a3c0455cd128ab1e5d1b5a749e409
Akshay Sarapure <akshaysarapure@gmail.com>
2025-02-18 12:45:09 +0530

    initial project setup + logo

--- MainApp/app/src/main/java/com/example/deepshield/MainActivity.kt
+++ MainApp/app/src/main/java/com/example/deepshield/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 
 import androidx.compose.ui.Modifier
 import com.example.deepshield.ui.theme.DeepShieldTheme
@@ -21,7 +22,7 @@ class MainActivity : ComponentActivity() {
             DeepShieldTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                    Box(modifier = Modifier.padding(innerPadding)){
-
+                     Text("Hello world")
                    }
                 }
             }
