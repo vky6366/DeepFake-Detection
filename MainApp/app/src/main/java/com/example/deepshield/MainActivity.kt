5468b82fd322386dd5683ff9638c60dad5f57bf8
Akshay Sarapure <akshaysarapure@gmail.com>
2025-02-18 12:38:43 +0530

    initial project setup + logo

--- MainApp/app/src/main/java/com/example/deepshield/MainActivity.kt
+++ MainApp/app/src/main/java/com/example/deepshield/MainActivity.kt
@@ -4,45 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.deepshield.Constants.Constants
 import com.example.deepshield.ui.theme.DeepShieldTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        //installSplashScreen()
         setContent {
             DeepShieldTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                   Box(modifier = Modifier.padding(innerPadding)){
+
+                   }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello ${Constants.API_KEY}!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DeepShieldTheme {
-        Greeting("Android")
-    }
-}
