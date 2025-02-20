e35dd588bb4d762a90c082603136c33625731342
Akshay Sarapure <akshaysarapure@gmail.com>
2025-02-20 13:09:45 +0530

    Added Function

--- MainApp/app/src/main/java/com/example/deepshield/domain/Repository/Repository.kt
+++ MainApp/app/src/main/java/com/example/deepshield/domain/Repository/Repository.kt
@@ -1,6 +1,11 @@
 package com.example.deepshield.domain.Repository
 
+import android.content.Context
+import com.example.deepshield.data.Response.DeepFakeVideoResponse
+import com.example.deepshield.domain.StateHandling.ApiResult
+import kotlinx.coroutines.flow.Flow
+
 interface Repository {
     //interface class
-    suspend fun uploadVideoToDeepFakeServer()
+    suspend fun uploadVideoToDeepFakeServer(context:Context,videoUrl:String): Flow<ApiResult<DeepFakeVideoResponse>>
 }
