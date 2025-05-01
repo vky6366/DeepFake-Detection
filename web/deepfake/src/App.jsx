9719d13c79400972e079675dd45ecd0d4eb40e21
Jyoti kamble <jyotikamble5235@gmail.com>
2025-05-01 17:34:54 +0530

    web

---
+++ web/deepfake/src/App.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import Header from "./components/Header";
+import Upload from "./components/Upload";
+import Loading from "./components/Loading";
+import Result from "./components/Result";
+import VisualAnalysis from "./components/VisualAnalysis";
+import axios from "./server/api";
+import './App.css'
+
+function App() {
+    const [loading, setLoading] = useState(false);
+    const [prediction, setPrediction] = useState(null);
+    const [frameImg, setFrameImg] = useState(null);
+    const [gradcamImg, setGradcamImg] = useState(null);
+
+    // Function to fetch visual analysis images
+    const fetchVisualAnalysis = async () => {
+        try {
+            const frameResponse = await axios.get("/get_3rd_frame");
+            const heatmapResponse = await axios.get(`/gradcam?timestamp=${new Date().getTime()}`);
+
+            // Processing  the 3rd frame
+            const frameBlob = new Blob([new Uint8Array(frameResponse.data.image_bytes)], { type: "image/jpeg" });
+            setFrameImg(URL.createObjectURL(frameBlob));
+
+            // Processing the Grad-CAM heatmap
+            const heatmapBlob = new Blob([new Uint8Array(heatmapResponse.data.heatmap_bytes)], { type: "image/jpeg" });
+            setGradcamImg(URL.createObjectURL(heatmapBlob));
+        } catch (error) {
+            console.error("Error fetching visual analysis:", error);
+            alert("Error retrieving visual analysis. Please try again.");
+        }
+    };
+
+    return (
+        <>
+            {/* displaying the result */}
+            <div className="gradient-bg min-h-screen flex flex-col items-center">
+                <Header />
+                <main className="container mx-auto px-6 py-12 flex flex-col gap-8 w-full max-w-3xl">
+                    <Upload setPrediction={setPrediction} setLoading={setLoading} fetchVisualAnalysis={fetchVisualAnalysis} />
+                    {loading && <Loading />}
+                    {prediction && <Result prediction={prediction} />}
+                    <VisualAnalysis frameImg={frameImg} gradcamImg={gradcamImg} />
+                </main>
+            </div>
+        </>
+    )
+}
+
+export default App
